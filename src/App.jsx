277ea95b3b4f27dbdcf3f35b0f89a8dbbe57fa11
Laiba Siddique Qureshi <157726748+laibasiddique75@users.noreply.github.com>
2024-12-15 17:55:30 +0500

    completed

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Card from "./Components/Card"
+import Footer from "./Components/Footer"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+
+// import './App.css'
+function App() {
+
+
+  return (
+    <>
+<Navbar/>;
+<Hero/>;
+<Card/>;
+<Footer/>;
+
+     </>
+  )
+}
+
+export default App
